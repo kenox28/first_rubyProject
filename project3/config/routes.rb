72e98a37392352d1cc79438b342dac9f75b13d89
kenox28 <iquenxzx@gmail.com>
2024-07-02 21:52:41 +0500

    learning bootrap

--- project3/config/routes.rb
+++ project3/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'web1#index2'
 
-  post '/formlogin', to: 'web1#login'
+  # post '/formlogin', to: 'web1#login'
 
   # other routes can be defined here
 end
-
